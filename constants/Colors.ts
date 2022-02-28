1f2cc1b68d33bc36a82cc6d7edba39b3b5d0bcca
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-28 17:27:18 +0500

    colors setup

---
+++ constants/Colors.ts
@@ -0,0 +1,11 @@
+export default {
+    primary: "#27ae60",
+    background_layout: "#F2F2F2",
+    background_card: "#FFFFFF",
+    background_progress_bar: "#3027ae60",
+    text_primary: "#000000",
+    text_error: "#E74C3C",
+    text_button: "#FFFFFF",
+    ripple_card: "#3027ae60",
+    ripple_button: "#30000000",
+}
