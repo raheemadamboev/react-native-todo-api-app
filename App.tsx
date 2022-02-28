84596ccfd08cef62bc9f52e9dcfacb5c3dc18c59
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-28 19:02:03 +0500

    creating card component

--- App.tsx
+++ App.tsx
@@ -4,25 +4,11 @@ import ProgressBar from "./components/ProgressBar";
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [todos, setTodos] = useState<ToDo[]>([]);
-  const [error, setError] = useState<string>("");
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((response) => response.json())
-      .then((json) => setTodos(json as ToDo[]))
-      .catch((error) => setError(error))
-      .finally(() => setLoading(false));
-  }, []);
+  
 
   return (
     <View style={styles.screen}>
-      <FlatList
-        data={todos}
-        renderItem={(item) => <Text>{item.item.title}</Text>}
-      />
-      <ProgressBar loading={loading} error={error} />
+     
     </View>
   );
 }
@@ -36,7 +22,7 @@ const styles = StyleSheet.create({
   },
 });
 
-type ToDo = {
+export type ToDo = {
   userId: number;
   id: number;
   title: string;
--- screens/ToDoList.tsx
+++ screens/ToDoList.tsx
@@ -1,8 +1,29 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
+import { useEffect, useState } from "react";
+import { ToDo } from "../App";
+import ProgressBar from "../components/ProgressBar";
 
 const ToDoList = (props: Props) => {
-  return <View style={styles.screen}></View>;
+  const [loading, setLoading] = useState<boolean>(true);
+  const [todos, setTodos] = useState<ToDo[]>([]);
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setTodos(json as ToDo[]))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return <View style={styles.screen}>
+       <FlatList
+        data={todos}
+        renderItem={(item) => <Text>{item.item.title}</Text>}
+      />
+      <ProgressBar loading={loading} error={error} />
+  </View>;
 };
 
 const styles = StyleSheet.create({
