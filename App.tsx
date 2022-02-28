46cb93bb85ab880883f9c44ad3754be9342afccf
raheemadamboev <vividricemaryandmax@gmail.com>
2022-02-28 17:38:41 +0500

    plain screen and app

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Colors from "./constants/Colors";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <View style={styles.screen}></View>;
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: Colors.background_layout,
   },
 });
---
+++ screens/ToDoList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+const ToDoList = (props: Props) => {
+  return <View style={styles.screen}></View>;
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+});
+
+export type Props = {};
+
+export default ToDoList;
